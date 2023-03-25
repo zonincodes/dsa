ef1273186144465b5f43a155245c1dd22b8b1522
zonin <business.kariukimartin@gmail.com>
2023-03-25 16:06:17 +0300

    function definations

--- the_library_program/libray.cpp
+++ the_library_program/libray.cpp
@@ -7,14 +7,17 @@ using namespace std;
 
 class Patron; // foward declaration
 
-class Book{
+class Book
+{
 
 public:
-    Book(){
+    Book()
+    {
         patron = 0;
     }
 
-    bool operator == (const Book &bk) const {
+    bool operator==(const Book &bk) const
+    {
         return strcmp(title, bk.title) == 0;
     }
 
@@ -22,7 +25,8 @@ private:
     char *title;
     Patron *patron;
     ostream &printBook(ostream &) const;
-    friend ostream &operator << (ostream &out, const Book &bk){
+    friend ostream &operator<<(ostream &out, const Book &bk)
+    {
         return bk.printBook(out);
     }
     friend Patron;
@@ -32,25 +36,26 @@ private:
     friend void returnBook();
 }; // book class end
 
-
 //  class Author
 
-class Author{
-public: 
-    Author(){}
+class Author
+{
+public:
+    Author() {}
 
-    bool operator == (const Author &ar) const {
+    bool operator==(const Author &ar) const
+    {
         return strcmp(name, ar.name) == 0;
     }
 
 private:
     char *name;
-    list <Book> books;
-    ostream &printOuthor (ostream &) const ;
-    
-    friend ostream &operator << (ostream &out, const Author &ar){
-        return ar.printOuthor(out);
+    list<Book> books;
+    ostream &printAuthor(ostream &) const;
 
+    friend ostream &operator<<(ostream &out, const Author &ar)
+    {
+        return ar.printAuthor(out);
     }
 
     friend void includeBook();
@@ -62,15 +67,18 @@ private:
 }; // Class Author end
 
 // datatype checkedoutBook start
-class CheckedOutBook{
+class CheckedOutBook
+{
 public:
-    CheckedOutBook(list<Author>::iterator ar, list<Book>::iterator bk){
+    CheckedOutBook(list<Author>::iterator ar, list<Book>::iterator bk)
+    {
         author = ar;
         book = bk;
     }
 
-    bool operator== (const CheckedOutBook &bk) const {
-        return strcmp(author -> name, bk.author -> name) == 0 && strcmp(book -> title, bk.book -> title) == 0;
+    bool operator==(const CheckedOutBook &bk) const
+    {
+        return strcmp(author->name, bk.author->name) == 0 && strcmp(book->title, bk.book->title) == 0;
     }
 
 private:
@@ -83,13 +91,15 @@ private:
 }; // datatype checkedoutBook end
 
 //  Patron class
-class Patron{
-public: 
-    Patron(){
-
+class Patron
+{
+public:
+    Patron()
+    {
     }
 
-    bool operator==(const Patron &pn) const {
+    bool operator==(const Patron &pn) const
+    {
         return strcmp(name, pn.name) == 0;
     }
 
@@ -97,7 +107,8 @@ private:
     char *name;
     list<CheckedOutBook> books;
     ostream &printPatron(ostream &) const;
-    friend ostream &operator<< (ostream &out, const Patron &pn){
+    friend ostream &operator<<(ostream &out, const Patron &pn)
+    {
         return pn.printPatron(out);
     }
 
@@ -106,3 +117,49 @@ private:
     friend Book;
 }; // end of patron class
 
+list<Author> catalog['Z' + 1];
+list<Patron> peaople['Z' + 1];
+
+ostream &Author::printAuthor(ostream &out) const
+{
+    out << name << endl;
+    list<Book>::const_iterator ref = books.begin();
+    for (; ref != books.end(); ref++)
+        out << *ref; // overloaded <<
+    return out;
+}
+
+ostream &Patron::printPatron(ostream &out) const
+{
+    out << name;
+    if (!books.empty())
+    {
+        out << " Hass the following books: \n";
+        list<CheckedOutBook>::const_iterator bk = books.begin();
+        for (; bk != books.end(); bk++)
+        {
+            out << "    * " << bk->author->name << ", " << bk->book->title << endl;
+        }
+    }
+    else
+        out << "has no books \n";
+    return out;
+}
+
+template <class T>
+ostream &operator<<(ostream &out, const list<T> &lst)
+{
+    for (list<T>::const_iterator ref = lst.begin; ref != lst.end(); ref++)
+        out << *ref; // overloaded <<
+    return out;
+}
+
+char *getString(char *msg){
+    char s[82], i, *destin;
+    cout << msg;
+    cin.get(s, 80);
+    while (cin.get(s[81]) && s[81] != '\n'); // discard overflowing
+    destin = new char[strlen(s) + 1];
+    for(i = 0; destin[i] == toupper(s[i]); i++);
+    return destin;
+}
