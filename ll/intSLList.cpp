8dbc0430d6bfeb6bf0fdc8637990be78658334de
zonin <business.kariukimartin@gmail.com>
2023-03-30 15:47:12 +0300

    delete from head

---
+++ ll/intSLList.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "intSLList.h"
+
+IntSingleLinkedList::~IntSingleLinkedList(){
+    for(IntSingleLinkedListNode *p; !isEmpty();){
+        head-> next;
+        delete head;
+        head = p;
+    }
+}
+
+void IntSingleLinkedList::addToHead(int el){
+    head = new IntSingleLinkedListNode(el, head);
+    if(tail == 0)
+        tail = head;
+}
+
+void IntSingleLinkedList::addToTail(int el){
+    if(tail != 0){
+        tail -> next = new IntSingleLinkedListNode(el);
+        tail = tail ->next;
+    }
+    else
+        head = tail = new IntSingleLinkedListNode(el);
+}
+
+int IntSingleLinkedList::deleteFromHead(){
+    int el = head ->info;
+    IntSingleLinkedListNode *temp = head;
+    if(head == tail) // if only node is the list;
+        head = tail = 0;
+    else 
+        head = head -> next;
+    
+    delete temp;
+    return el;
+}
